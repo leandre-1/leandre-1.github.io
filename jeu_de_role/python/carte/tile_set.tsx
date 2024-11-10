5de24e1939d1833a3df1596df8f2bca27fdee71c
Léandre <leandre.temperault@gmail.com>
2024-11-10 16:08:29 +0100

    a

--- jeu_de_role/python/carte/tile_set.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="base" tilewidth="32" tileheight="32" tilecount="667" columns="23">
- <image source="data/base.png" width="736" height="928"/>
-</tileset>
